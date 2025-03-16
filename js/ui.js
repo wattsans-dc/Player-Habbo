80e2b6ce956492607d15799dc124ba456cbf5160
worldsofk3 <qdmk3ntin@gmail.com>
2025-03-16 14:11:43 +0100

    Fixs CSS

--- js/ui.js
+++ js/ui.js
@@ -32,7 +32,7 @@
   
   // Réouvrir le player
   document.getElementById("openPlayer").addEventListener("click", function(){
-    document.getElementById("playerContainer").style.display = "block";
+    document.getElementById("playerContainer").style.display = "flex";
     document.getElementById("openPlayer").style.display = "none";
   });
 })();
