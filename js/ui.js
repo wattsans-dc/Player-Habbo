424f22fc033adb4826048f1a10cc53a937e795ef
wattsans <wattsans@gmail.com>
2025-03-16 12:58:05 +0100

    affichage player

--- js/ui.js
+++ js/ui.js
@@ -1,27 +1,39 @@
 (function(){
-    var container = document.getElementById("playerContainer");
-    var isDragging = false;
-    var offsetX, offsetY;
-    container.addEventListener("mousedown", function(e){
-      var rect = container.getBoundingClientRect();
-      var resizeZone = 20;
-      if(e.clientX > rect.right - resizeZone && e.clientY > rect.bottom - resizeZone) return;
-      if(e.target.tagName !== "INPUT" && e.target.tagName !== "BUTTON"){
-        isDragging = true;
-        offsetX = e.clientX - rect.left;
-        offsetY = e.clientY - rect.top;
-        container.style.cursor = "grabbing";
-      }
-    });
-    document.addEventListener("mousemove", function(e){
-      if(isDragging){
-        container.style.left = (e.clientX - offsetX) + "px";
-        container.style.top = (e.clientY - offsetY) + "px";
-      }
-    });
-    document.addEventListener("mouseup", function(){
-      isDragging = false;
-      container.style.cursor = "grab";
-    });
-  })();
+  var container = document.getElementById("playerContainer");
+  var isDragging = false;
+  var offsetX, offsetY;
   
+  container.addEventListener("mousedown", function(e){
+    var rect = container.getBoundingClientRect();
+    var resizeZone = 20;
+    if(e.clientX > rect.right - resizeZone && e.clientY > rect.bottom - resizeZone) return;
+    if(e.target.tagName !== "INPUT" && e.target.tagName !== "BUTTON"){
+      isDragging = true;
+      offsetX = e.clientX - rect.left;
+      offsetY = e.clientY - rect.top;
+      container.style.cursor = "grabbing";
+    }
+  });
+  
+  document.addEventListener("mousemove", function(e){
+    if(isDragging){
+      container.style.left = (e.clientX - offsetX) + "px";
+      container.style.top = (e.clientY - offsetY) + "px";
+    }
+  });
+  
+  document.addEventListener("mouseup", function(){
+    isDragging = false;
+    container.style.cursor = "grab";
+  });
+  
+  document.getElementById("closePlayer").addEventListener("click", function(){
+    document.getElementById("playerContainer").style.display = "none";
+    document.getElementById("showPlayer").style.display = "block";
+  });
+  
+  document.getElementById("showPlayer").addEventListener("click", function(){
+    document.getElementById("playerContainer").style.display = "block";
+    this.style.display = "none";
+  });
+})();
--- js/youtubePlayer.js
+++ js/youtubePlayer.js
@@ -1,41 +1,54 @@
 (function() {
-    var player;
-    function getPlayer() {
-      return player;
+  var player;
+  function getPlayer() {
+    return player;
+  }
+  function loadVideo(videoID, playlistID, index) {
+    var audioOnly = document.getElementById('audioOnly').checked;
+    var embedUrl = "https://www.youtube.com/embed/" + videoID + "?enablejsapi=1&autoplay=1";
+    if (playlistID) {
+      embedUrl += "&list=" + playlistID;
+      if (index) {
+        embedUrl += "&index=" + index;
+      }
     }
-    function loadVideo(videoID, playlistID, index) {
-      var audioOnly = document.getElementById('audioOnly').checked;
-      var embedUrl = "https://www.youtube.com/embed/" + videoID + "?enablejsapi=1&autoplay=1";
-      if (playlistID) {
-        embedUrl += "&list=" + playlistID;
-        if (index) {
-          embedUrl += "&index=" + index;
-        }
-      }
-      if (audioOnly) {
-        embedUrl += "&controls=0&showinfo=0&rel=0&modestbranding=1";
-      }
-      var height = audioOnly ? "0" : "150";
-      document.getElementById("results").innerHTML = '<iframe id="ytPlayer" width="300" height="'+ height +'" src="'+ embedUrl +'" frameborder="0" allow="autoplay"></iframe>';
-      setTimeout(function(){
-        player = document.getElementById("ytPlayer");
-      }, 1000);
+    if (audioOnly) {
+      embedUrl += "&controls=0&showinfo=0&rel=0&modestbranding=1";
     }
-    document.getElementById("submitFormData").addEventListener("click", function(){
-      var url = document.getElementById("video").value;
-      if(url){
-        var videoID = window.extractVideoID(url);
-        var playlistID = window.extractPlaylistID(url);
-        var index = window.extractIndex(url);
-        loadVideo(videoID, playlistID, index);
+    var height = audioOnly ? "0" : "150";
+    document.getElementById("results").innerHTML = '<iframe id="ytPlayer" width="300" height="'+ height +'" src="'+ embedUrl +'" frameborder="0" allow="autoplay"></iframe>';
+    setTimeout(function(){
+      player = document.getElementById("ytPlayer");
+    }, 1000);
+  }
+  function checkCopyright(videoID){
+    return videoID && videoID.charAt(0) === "c";
+  }
+  function askGoogleAccount(){
+    if(!localStorage.getItem("googleLinked")){
+      var email = prompt("Veuillez lier votre compte Google pour lire cette musique protégée par des droits d'auteur.");
+      if(email){
+        localStorage.setItem("googleLinked", email);
       }
-    });
-    document.getElementById("audioOnly").addEventListener("change", function(){
-      var audioOnly = this.checked;
-      if(player){
-        player.style.height = audioOnly ? "0px" : "150px";
+    }
+  }
+  document.getElementById("submitFormData").addEventListener("click", function(){
+    var url = document.getElementById("video").value;
+    if(url){
+      var videoID = window.extractVideoID(url);
+      if(checkCopyright(videoID)){
+        askGoogleAccount();
       }
-    });
-    window.getPlayer = getPlayer;
-  })();
-  
+      var playlistID = window.extractPlaylistID(url);
+      var index = window.extractIndex(url);
+      loadVideo(videoID, playlistID, index);
+    }
+  });
+  document.getElementById("audioOnly").addEventListener("change", function(){
+    var audioOnly = this.checked;
+    if(player){
+      player.style.height = audioOnly ? "0px" : "150px";
+    }
+  });
+  window.getPlayer = getPlayer;
+})();
